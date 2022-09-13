0fb7fec27bf00f7f83aab714a879cc154d5c1a20
agus-0402 <agusjcr2016@gmail.com>
2022-09-13 09:37:56 -0300

    se agregaron cositas

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 
 const app: Express = express();
@@ -12,6 +12,25 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(process.env.PORT, () => {
   console.log(`Server is up and running at port ${process.env.PORT}`);
 });
+
+/*const middleware = (req, res , next) => {
+
+}¨*/
+
+app.get("/", /*middleware,*/(req: Request, res: Response) => {
+res.send("TITO TE QUIERO <3")   
+})
+
+interface User{
+  name: string
+  lastname: string
+}
+const Users: User[] = [{name: "agustin", lastname: "sanchez"}]
+
+app.post("/", (req: Request<any, any, User>, res) => {
+ Users.push(req.body)
+ res.send(JSON.stringify({data: Users}))
+})
 
 connectToDb()
   .then(() => console.log("Database connected successfully"))
